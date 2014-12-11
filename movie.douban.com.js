e8bcab4120f3c8fd1dcd1d92c3e0e9d47800187d
Joey <qiaoxueshi@gmail.com>
2014-12-12 01:16:09 +0800

    some fix on movie.douban.com.js

--- movie.douban.com.js
+++ movie.douban.com.js
@@ -1,23 +1,30 @@
 
 function appendEleToDiv(div, url, urlName, movieName) {
-	//remove suffix likes "(2014)"
-	var indexOfParenthesis = movieName.indexOf("(");  
+	div.append("<br /><a class='pl' target='_blank' href='" + encodeURI (url  + movieName) + "'> " + urlName + "</a>");
+}
+
+function removeParenthesisAndTrim(text) {
+	var indexOfParenthesis = text.indexOf("(");  
 	if (indexOfParenthesis > 0) {
-		movieName = movieName.substring(0, indexOfParenthesis); 
+		text = text.substring(0, indexOfParenthesis); 
 	}
-	div.append("<br /><a class='pl' target='_blank' href='" + url  + encodeURI(movieName) + "'> " + urlName + "</a>");
+
+	return text.trim();
 }
 
 //debugger;
-var movieName = $("#content h1 span").text();
-if (movieName != undefined && movieName.length > 0) {
-	console.log("INFO! movie.douban.com.js: find movie name :" + movieName);
+var chineseName = removeParenthesisAndTrim($("title").text());
+var movieName = removeParenthesisAndTrim($("#content h1 span").text());
+
+if (chineseName != undefined && chineseName.length > 0 && movieName != undefined && movieName.length > 0) {
+	var chineseNameEndIndex = movieName.indexOf(chineseName) + chineseName.length;
+	var englishName = movieName.substring(chineseNameEndIndex).trim();
 
 	var infoDiv = $("#info");
-	appendEleToDiv(infoDiv, "http://www.bttiantang.com/s.php?q=", 				"下载：BT天堂", 					movieName);
-	appendEleToDiv(infoDiv, "https://kickass.so/usearch/", 						"下载：KickAss", 				movieName);
-	appendEleToDiv(infoDiv, "http://s.kujian.com/plus/search.php?keyword=", 	"下载：电影天堂", 				movieName);
-	appendEleToDiv(infoDiv, "http://subhd.com/search/", 						"字幕下载：subhd.com", 			movieName);
+	appendEleToDiv(infoDiv, "http://www.bttiantang.com/s.php?q=", 				"下载：BT天堂", 					chineseName);
+	appendEleToDiv(infoDiv, "https://kickass.so/usearch/", 						"下载：KickAss", 				englishName.length > 0 ? englishName : chineseName);
+	appendEleToDiv(infoDiv, "http://s.kujian.com/plus/search.php?keyword=", 	"下载：电影天堂", 				chineseName);
+	appendEleToDiv(infoDiv, "http://subhd.com/search/", 						"字幕下载：subhd.com", 			chineseName);
 } else {
 	console.log("WARNING! movie.douban.com.js: Can't find movie name.");
 }
