a23f4277201ce1e5afca51f8918335ab3bb3725a
Joey <qiaoxueshi@QiaoXueshis-MacBook-Pro.local>
2014-12-22 17:33:16 +0800

    update tower.im.js

--- tower.im.js
+++ tower.im.js
@@ -4,10 +4,10 @@ var nav = $("ul.nav");
 
 // debugger;
 
-nav.append("<li id='nav-team1'><a style='color:#DBA8BA;' href='/teams/9880514921ca4ae196eca2e090d4065f/projects/' data-stack='' data-stack-root=''> ｜ </a></li>");
+nav.append("<li id='nav-team1'><a style='color:#C6D5A4;' href='/teams/9880514921ca4ae196eca2e090d4065f/projects/' data-stack='' data-stack-root=''> ｜ </a></li>");
 
-//开发团队
-nav.append("<li id='nav-team1'><a style='color:#8CB6C7;' href='/teams/9880514921ca4ae196eca2e090d4065f/projects/' data-stack='' data-stack-root=''>开发团队</a></li>");
+//Fenice开发项目
+nav.append("<li id='nav-team1'><a style='color:#8CB6C7;' href='/projects/5e730782c3b04d819cb734641b06f9de/' data-stack='' data-stack-root=''>Dev</a></li>");
 
-//团队
-nav.append("<li id='nav-team1'><a style='color:#C6D5A4;' href='/teams/5e745676bffb4cbfaf8743a380994334/projects/' data-stack='' data-stack-root=''>客服团队</a></li>");
+//Fenice客服项目
+nav.append("<li id='nav-team1'><a style='color:#DBA8BA;' href='/projects/3196129154d545b1a50ca9ab0140815b/' data-stack='' data-stack-root=''>Bug</a></li>");
